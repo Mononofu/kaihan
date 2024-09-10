c0e622177631fc4b8ef25e46730361a8bb6f0a10
Julian Schrittwieser <j.schrittwieser@gmail.com>
2024-09-10 12:56:24 +0100

    support jinja template rendering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::{anyhow, bail, Result};
+use log::{error, info};
 use pulldown_cmark::{Event, Tag, TagEnd};
-use std::collections::HashMap;
+use serde::Serialize;
+use std::collections::{HashMap, HashSet};
 use std::fs;
-use std::os::macos::raw::stat;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 #[derive(PartialEq, Debug)]
@@ -95,7 +97,23 @@ fn read_source_files(current: &Path, prefix: &Path) -> Result<Vec<RawFile>> {
                     let path = if let Some(p) = metadata.get("save_as") {
                         Path::new(p).to_path_buf()
                     } else {
-                        prefix.join(path.file_stem().unwrap())
+                        let slug = metadata
+                            .get("title")
+                            .unwrap()
+                            .chars()
+                            .filter(|c| c.is_alphanumeric() || c.is_ascii_whitespace())
+                            .collect::<String>()
+                            .to_ascii_lowercase()
+                            .replace(' ', "-");
+                        if metadata.get("layout").unwrap_or(&String::new()) == "page" {
+                            PathBuf::new().join(slug)
+                        } else {
+                            PathBuf::new()
+                                .join("blog")
+                                .join(date.format("%Y/%m/%d").to_string())
+                                .join(slug)
+                        }
+                        // prefix.join(path.file_stem().unwrap())
                     };
                     let tags = if let Some(tag_str) = metadata.get("tags") {
                         tag_str
@@ -132,13 +150,12 @@ fn read_source_files(current: &Path, prefix: &Path) -> Result<Vec<RawFile>> {
     Ok(files)
 }
 
-fn render_content(f: &RawContent, output_path: &Path) -> Result<()> {
-    if let Some(s) = f.metadata.get("status") {
-        if s == "draft" {
-            return Ok(());
-        }
-    }
-
+fn render_content(
+    f: &RawContent,
+    output_path: &Path,
+    jinja: &minijinja::Environment,
+    validate_local_urls: bool,
+) -> Result<()> {
     let title = f.metadata.get("title").ok_or(anyhow!("Must have title!"))?;
 
     let style_css = std::fs::read_to_string(
@@ -155,21 +172,35 @@ fn render_content(f: &RawContent, output_path: &Path) -> Result<()> {
 
     let parser = pulldown_cmark::Parser::new_ext(&f.markdown, options);
 
-    // TODO(swj): Support link archiving.
     let parser = parser.map(|e| match e {
         Event::Start(pulldown_cmark::Tag::Link {
             link_type,
             dest_url,
             title,
             id,
-        }) => Event::Start(pulldown_cmark::Tag::Link {
-            link_type,
-            dest_url: pulldown_cmark::CowStr::Boxed(
-                dest_url.trim_start_matches("!").to_owned().into_boxed_str(),
-            ),
-            title,
-            id,
-        }),
+        }) => {
+            // TODO(swj): Support link archiving.
+            let url = dest_url.trim_start_matches("!").to_owned();
+
+            // Verify that internal links are valid.
+            if url.starts_with("/") {
+                let url = url.trim_matches('/');
+                // Strip # anchor links.
+                let url = url.split_once('#').map(|(a, b)| a).unwrap_or(url);
+                if validate_local_urls && !output_path.join(&url).exists() {
+                    error!("Dangling internal URL in {:?}: {:}, expected {:?}", f.path, url, output_path.join(&url));
+                }
+            } else if !url.starts_with("http") && !url.starts_with("mailto") {
+                error!("Interal URLs should be absolute {:?}, external URLs should start with https://, got: {:}", f.path, url);
+            }
+
+            Event::Start(pulldown_cmark::Tag::Link {
+                link_type,
+                dest_url: pulldown_cmark::CowStr::Boxed(url.into_boxed_str()),
+                title,
+                id,
+            })
+        }
         _ => e,
     });
 
@@ -211,7 +242,7 @@ fn render_content(f: &RawContent, output_path: &Path) -> Result<()> {
     } else {
         output_path.to_path_buf()
     };
-    let dst = output_path.join(&f.path).with_extension("html");
+    let dst = output_path.join(&f.path).join("index.html");
 
     std::fs::create_dir_all(dst.parent().unwrap())?;
 
@@ -262,15 +293,64 @@ fn render_content(f: &RawContent, output_path: &Path) -> Result<()> {
     Ok(())
 }
 
+fn read_templates(template_path: &Path) -> Result<minijinja::Environment> {
+    let mut env = minijinja::Environment::new();
+
+    for path in glob::glob(template_path.join("**/*").to_str().unwrap())? {
+        let path = path?;
+        if path.is_file() {
+            let name = path.strip_prefix(template_path)?.to_str().unwrap();
+            info!("loading template {path:?} as {name:?}");
+            env.add_template_owned(name.to_owned(), std::fs::read_to_string(&path)?)?;
+        }
+    }
+
+    Ok(env)
+}
+
+#[derive(Serialize, Debug)]
+struct ArticlesPage {
+    object_list: Vec<Article>,
+}
+
+#[derive(Serialize, Debug)]
+struct Article {
+    title: String,
+    url: String,
+    summary: String,
+}
+
 fn main() -> Result<()> {
-    let blog_path = Path::new("/Users/mononofu/Dropbox/blog/content/");
+    // Include log level, current time and file:line in each log message.
+    env_logger::Builder::from_default_env()
+        .format(|buf, record| {
+            let style = buf.default_level_style(record.level());
+
+            writeln!(
+                buf,
+                "[{}{}{:#}] {} {}:{} {}",
+                style,
+                record.level(),
+                style,
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                record.args(),
+            )
+        })
+        .init();
+
+    let blog_path = Path::new("/Users/mononofu/Dropbox/blog/");
+    let content_path = blog_path.join("content");
+    let templates_path = blog_path.join("themes/svbhack/templates");
 
     let render_path = Path::new("/Users/mononofu/tmp/blog/");
 
     std::fs::remove_dir_all(render_path)?;
     std::fs::create_dir_all(render_path)?;
 
-    let files = read_source_files(blog_path, Path::new(""))?;
+    let files = read_source_files(&content_path, Path::new(""))?;
+    let jinja = read_templates(&templates_path)?;
 
     let mut by_layout: HashMap<&str, Vec<&RawContent>> = HashMap::new();
     let mut by_tag: HashMap<&str, Vec<&RawContent>> = HashMap::new();
@@ -295,14 +375,48 @@ fn main() -> Result<()> {
         println!("{:} {:}s", entries.len(), layout);
     }
 
-    for f in files {
+    let articles = by_layout
+        .get("post")
+        .unwrap()
+        .iter()
+        .rev()
+        .take(10)
+        .map(|p| Article {
+            title: p.metadata.get("title").unwrap().clone(),
+            url: p.path.to_str().unwrap().to_owned(),
+            summary: "todo".to_string(),
+        })
+        .collect();
+
+    let base_context = minijinja::context! {
+        AUTHOR => "Julian Schrittwieser",
+        SITENAME => "furidamu",
+        SITEURL => "",
+        USER_LOGO_URL => "/images/me_2018_11_01.webp",
+    };
+
+    let tmpl = jinja.get_template("index.html")?;
+    let index = tmpl.render(minijinja::context! {
+    articles_page =>  ArticlesPage{object_list: articles},
+    ..base_context})?;
+    std::fs::write(render_path.join("index.html"), index)?;
+
+    // Run once to render and save.
+    for f in files.iter() {
         match f {
-            RawFile::Content(c) => render_content(&c, render_path)?,
+            RawFile::Content(c) => render_content(&c, render_path, &jinja, false)?,
             RawFile::Static(i) => {
-                let dst = render_path.join(i.path);
+                let dst = render_path.join(&i.path);
                 std::fs::create_dir_all(dst.parent().unwrap())?;
                 std::fs::write(dst, &i.data)?;
             }
+        }
+    }
+
+    // Run again to verify internal links.
+    for f in files.iter() {
+        if let RawFile::Content(c) = f {
+            render_content(&c, render_path, &jinja, true)?;
         }
     }
 
